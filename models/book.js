bbc699b33f34d665aad86c44ccfcb924bd3dbf61
Allyse <ayubired@gmail.com>
2020-12-17 23:48:42 -0600

    updated key values

--- models/book.js
+++ models/book.js
@@ -4,8 +4,8 @@ const Schema = mongoose.Schema;
 const googlebooks = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  description: String,
+  image: { type: Date, default: Date.now }
 });
 
 const Book = mongoose.model("Book", googlebooks);
