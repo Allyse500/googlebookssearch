4767897b1ad44b9ddcb3627ffd55710fc3e5e726
Allyse <ayubired@gmail.com>
2020-12-18 16:23:08 -0600

    restyle backgrond

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path={["/", "/books"]}>
-            <Books />
-          </Route>
-          <Route exact path="/books/:id">
-            <Detail />
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div>
+        <Router>
+          <Navbar />
+          <div className="App">
+          <div className="App-header">
+          <h2>Google Books Search</h2>
+          </div>
+          <p className="App-intro">
+            Search for and save books.
+          </p>
+          </div>
+        </Router>
+    </div>
   );
 }
+
 
 export default App;
