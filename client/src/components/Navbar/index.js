215397876ced0fc665a7000ab33da3c71d903ff6
Allyse <ayubired@gmail.com>
2021-12-27 20:16:45 -0600

    updated CSS for screen sizing and some for prompts

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -24,6 +24,7 @@ function Navbar() {
      + response.data.items[0].volumeInfo.previewLink + "</a>" + "</div></div></div>");
     document.getElementById("content").innerHTML = myJSON;
   
+    document.getElementById('closeRsp').style.display = "block";
     document.getElementById('save').style.display = "block";
     document.getElementById('savedmsg').style.display = "none";
   
@@ -38,43 +39,78 @@ function save () {
     });
     document.getElementById('savedmsg').style.display = "block";
   };
-//===================CLOSE FUNCTION FOR DIALOG BOX====================================//
-  function close () {
-    document.getElementById('dialogbox').style.display = 'none';
-    document.getElementById('dialogoverlay').style.display = 'none';
-  }
 
-//================ABOUT DIALOG BOX DISPLAY FUNCTION=================================//
-  function about () {
-    document.getElementById('dialogbox').style.display = 'block';
-    document.getElementById('dialogoverlay').style.display = 'block';
-  }
+function closeRsp(){
+  document.getElementById("searchResultGrid").style.display="none";
+}
 
-//=================CANCEL FUNCTION FOR SIGN-IN AND REGISTER BOXES=======================//
-  function cancel () {
-    document.getElementById('signin').style.display = 'none';
-    document.getElementById('dialogoverlay').style.display = 'none';
-    document.getElementById('searchfield').style.display = 'flex';
-  }
+//==========================NAVBAR===================================================//
+function dropDownNav() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-//==============BEGIN LOGIN FUNCTION FOR SIGN-IN=========================================//
-  function beginlogin () {//need to enable dialog overlay
-    document.getElementById('dialogoverlay').style.display ="block";
-    document.getElementById('dialogoverlay').style.height ="auto";
-    document.getElementById('signin').style.display = 'block';
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
   }
+}
+
+//==========================ABOUT PROMPT BOX====================================//
+//display about prompt box-------------------------------------------------------------
+function aboutPromptBox(){
+  document.getElementById("aboutModal").style.display ="block";
+  document.getElementById("searchResultGrid").style.display="none";
+  }//end of aboutPromptBox()
+  
+  //close about prompt box-------------------------------------------------------------
+  function closeabout(){
+  document.getElementById("aboutModal").style.display ="none";
+  }//end of closeabout()
+
+//==========================SIGN UP PROMPT BOX===================================//
+//display signup prompt box-------------------------------------------------------------
+function signupPromptBox(){
+  document.getElementById("signupModal").style.display ="block";
+  document.getElementById("searchResultGrid").style.display="none";
+  }//end of signupPromptBox()
+  
+  //close signup prompt box-------------------------------------------------------------
+  function closesignup(){
+  document.getElementById("signupModal").style.display ="none";
+  }//end of closesignup()
+  
+
+//===========================LOGIN PROMPT BOX====================================//
+//display login prompt box-------------------------------------------------------------
+function loginPromptBox(){
+  document.getElementById("loginModal").style.display ="block";
+  document.getElementById("searchResultGrid").style.display="none";
+  }//end of loginPromptBox()
+  
+  //close login prompt box-------------------------------------------------------------
+  function closelogin(){
+  document.getElementById("loginModal").style.display ="none";
+  }//end of closelogin()
 
 
 
   return (
     <div>
       <div id="navbar">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div>
-            <ul className="navbar-nav">
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div> */}
+            {/* <ul className="navbar-nav"> */}
 
               <Link className="navbar-brand" to="/" id= "title" style = {{color: "white", fontSize: "180%", textDecoration: "none"}}>
-                Google Books Search
+                G<span className="abrev">oogle</span> B<span className="abrev">ooks</span> S<span className="abrev">earch</span>
               </Link>
               {/* <Link
                 to="/saved" id = "saved"
@@ -84,86 +120,93 @@ function save () {
                 Saved
               </Link> */}
 
-              <div id = "form">
-                <form className="search">
-                  <div className="form-group">
-                    <label htmlFor="language" style={{fontSize: "129%"}}>Search Term:</label>
-                      <input
-                      name="term"
-                      list="term"
-                      type="text"
-                      className="form-control"
-                      placeholder="Type in a search term to begin"
-                      id="term"
-                      />
+              <form id = "form">
+                <input id="term" name="term" list="term" type="text" placeholder="Type in a search term to begin"/>
+                <div id = "searchbtn" onClick = {search}>Search</div>
+                <div className="dropdown">
+                <div onClick={dropDownNav} className="dropbtn">&equiv;</div>
+                <div id="myDropdown" className="dropdown-content">
+                    <div id="aboutBtnDD" className="navOpt" onClick ={aboutPromptBox}>About</div>
+                    <div id="signUpBtnDD" className="navOpt" onClick={signupPromptBox}>Sign Up</div>
+                    <div id="loginBtnDD" className="navOpt" onClick = {loginPromptBox}>Log In</div>
+                </div>
+            </div>
+              </form>
+                
+              <div id="headerBtnGrid">
+                <button id = "about" onClick ={aboutPromptBox}>About</button>
+                <button id = "signup" onClick={signupPromptBox}>Sign Up</button>
+                <button id = "login" onClick = {loginPromptBox}>Login</button>
+              </div>
+            {/* </ul>         */}
+          {/* </div>      
+        </nav> */}
+      </div>
+      
+      <div id="searchResultGrid">
+        <div id ="content">
+        </div><div id='closeRsp' onClick={closeRsp}>Close</div>
+      </div>
+      {/* <br></br>
+      <button id = "save" onClick = {save}>Save</button>
+      <div id = "savedmsg">Saved!</div> */}
+{/* ABOUT DIALOG BOX============================================================= */}
+    <div id="aboutModal">
+        <div id="aboutContainer">
+            <div id="aboutSecondaryContainer">
+                <div id="aboutTitle">About</div>
+                <div id="aboutContents">
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span style={{color:"orange", fontWeight: "bold"}}>Welcome to Google Books Search!</span> Here, you may find any title of books hosted from Google Books. With an account, you may prepare a collection of books searched. This service is free to the public.<br/><br/> Enjoy!<br/><br/><span style={{fontStyle:"italic"}}>-Admin</span>
+                </div>
+                <div id="aboutCloseButton" onClick={closeabout}>Close</div>
+            </div>
+        </div>
+    </div>
+
+{/* SIGN UP DIALOG BOX=========================================================== */}
+    <div id="signupModal">
+        <div id="signupFormContainer">
+            <div id="signupFormSecondaryContainer">
+                <div id="signupFormTitle">Sign Up</div>
+                <form id="signupForm">
+                    <div id="signupInputContents">
+                        <label for="newUsername" className="signupFormLabel">Username: </label>
+                        <input className="formInput" type="text" id="newUsername" name="newUsername"/><br></br>
+                        <div id="signupPasswordBlock">
+                            <label for="newPassword" className="signupFormLabel">Password: </label>
+                            <input className="formInput" type="password" id="newPassword" name="newPassword"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div id="signupBtnsGrid">
+                        <button id="signupSubmitBtn">Submit</button><div id="signupCloseButton" onClick={closesignup}>Close</div>
                     </div>
                 </form>
-                <button id = "searchbtn" onClick = {search}>Search</button> 
-              </div>
-
-              <button id = "about" onClick ={about}>About</button>
-
-              <Link
-                to="/saved" id = "saved2"
-                className={window.location.pathname === "/saved" ? "nav-link active" : "nav-link"}
-                style = {{color: "white", fontSize: "130%", textDecoration: "none"}}
-              >
-              Sign Up
-              </Link>
-
-              <button id = "login" onClick = {beginlogin}>Login</button>
-
-            </ul>        
-          </div>      
-        </nav>
-      </div>
-
-      <div id ="content">
-      </div>
-      <br></br>
-      <button id = "save" onClick = {save}>Save</button>
-      <div id = "savedmsg">Saved!</div>
-{/* ABOUT DIALOG BOX============================================================= */}
-      <div id ="dialogoverlay"></div>
-      <div id= "dialogbox">
-        <div>
-          <div id ="dialogboxhead" style = {{textAlign: "center"}}>About</div>
-          <div id ="dialogboxbody">Welcome to Google Books Search! On this page, you can search for any book title provided by Google. Login or sign-up to save your book selections to build your own collection.</div>
-          <div id ="dialogboxfoot">
-            <button onClick={close} id ="aboutCB">Close</button>
-          </div>
+            </div>
         </div>
-      </div>
-{/* LOGIN DIALOG BOX============================================================= */}
-<div className="Login" id= "signin">
-      <div>
-        <div id ="signinhead">Login</div>
-          <div id ="signinbody">
-
-      <form> {/*Changed Form--> form*/}
-        <div size="lg" controlId="email">
-          <label>Email</label>
-          <input
-            id = "sie"
-            type="text"
-            style={{borderRadius: "5px", transform: "trasnlateY(-15%)"}}             
-          />
-        </div>
-        <div size="lg" controlId="password">
-          <label>Password</label>
-          <input
-            id = "sip"
-            type="text"
-            style={{borderRadius: "5px", transform: "trasnlateY(-15%)"}}
-          />
-        </div>
-        <button id ="loginsubmit" block size="lg" type="submit" style = {{transform: "translate(200px, 12px)"}}>
-          Login
-        </button>
-        <button id = "loginclose" onClick={cancel} style = {{transform: "translate(210px, 12px)"}}>Close</button>
-      </form>
-      </div>
     </div>
+
+{/* LOGIN DIALOG BOX============================================================= */}
+<div id="loginModal">
+        <div id="loginFormContainer">
+            <div id="loginFormSecondaryContainer">
+                <div id="loginFormTitle">Login</div>
+                <form id="loginForm">
+                    <div id="loginInputContents">
+                        <label for="username" className="loginFormLabel">Username: </label>
+                        <input className="formInput" type="text" id="username" name="username"/><br></br>
+                        <div id="loginPasswordBlock">
+                            <label for="password" className="loginFormLabel">Password: </label>
+                            <input className="formInput" type="password" id="password" name="password"/>
+                        </div>
+                    </div>
+                    <br></br>
+                    <div id="loginBtnsGrid">
+                        <button id="loginSubmitBtn">Submit</button><div id="loginCloseButton" onClick={closelogin}>Close</div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
 
